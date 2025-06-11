1fcc5cd23ff55894f896a40480a7de34e5c68d79
Gowthamap <gowtham.p@vonnue.com>
2025-06-11 19:03:12 +0530

    feat: While loop

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,14 @@
-// for loops
+// while loops
+const names = ["shaun", "mario", "luigi"];
+// let i = 0;
 
-// for (let i = 0; i < 5; i++) {
-//   console.log("in loop:", i);
+// while (i < 5) {
+//   console.log("in loop: ", i);
+//   i++;
 // }
 
-// console.log("loop finished");
-
-const names = ["shaun", "mario", "luigi"];
-
-for (let i = 0; i < names.length; i++) {
-  // console.log(names[i]);
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
+let i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
 }

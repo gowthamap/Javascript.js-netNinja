ae52aa4da3bc152ccc5a0b6ff4020b07a5a3478e
Gowthamap <gowtham.p@vonnue.com>
2025-06-12 16:01:24 +0530

    do while loop

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,8 @@
-// while loops
-const names = ["shaun", "mario", "luigi"];
-// let i = 0;
+// do while loops
 
-// while (i < 5) {
-//   console.log("in loop: ", i);
-//   i++;
-// }
+let i = 3;
 
-let i = 0;
-while (i < names.length) {
-  console.log(names[i]);
+do {
+  console.log("val of i is: ", i);
   i++;
-}
+} while (i < 5);

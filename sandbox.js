c143ce11eeac78a7469c1cd325bf247e6d2e78a7
Gowthamap <gowtham.p@vonnue.com>
2025-06-16 18:12:02 +0530

    feat: variables and block scope and local scope.

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,17 @@
-// switch statements
-const grade = "50";
+// variables & block scope
+const age = 30;
+// age = 50;
 
-switch (grade) {
-  case "50":
-    console.log("you got an A!");
-    break;
-  case "B":
-    console.log("you got an B!");
-    break;
-  case "C":
-    console.log("you got an C!");
-    break;
-  case "D":
-    console.log("you got an D!");
-    break;
-  case "E":
-    console.log("you got an E!");
-    break;
-  default:
-    console.log("not a valid grade");
+if (true) {
+  const age = 40;
+  const names = "shaun";
+  console.log("inside 1st code block: ", age, names);
+
+  if (true) {
+    const age = 50;
+    console.log("inside 2nd code bloc: ", age);
+    var test = "hello";
+  }
 }
+
+console.log("outside code block: ", age, names, test);

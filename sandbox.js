c9fa192bf2a1c86e5f1171b295a73e7f5a67fc20
Gowthamap <gowtham.p@vonnue.com>
2025-07-27 14:16:23 +0530

    functions

--- sandbox.js
+++ sandbox.js
@@ -1,41 +1,13 @@
-// variables & block scope
-const age = 30;
-// age = 50;
+const name = "shaun";
 
-// if (true) {
-//   const age = 40;
-//   const names = "shaun";
-//   // console.log("inside 1st code block: ", age, names);
+// functions
 
-//   if (true) {
-//     const age = 50;
-//     // console.log("inside 2nd code bloc: ", age);
-//     var test = "hello";
-//   }
-// }
+const greet = () => "hello";
 
-// // console.log("outside code block: ", age, names, test);
+let resultOne = greet();
+console.log(resultOne);
 
-// // practise arrow functions
+// methods
 
-// const greet = () => "hello, world";
-// const result = greet();
-// console.log(result);
-
-// const bill = function (products, tax) {
-//   let total = 0;
-//   for (let i = 0; i < products.length; i++) {
-//     total = products[i] + products[i] * tax;
-//   }
-//   return total;
-// };
-
-const bill = (products, tax) => {
-  let total = 0;
-  for (let i = 0; i < products.length; i++) {
-    total = products[i] + products[i] * tax;
-  }
-  return total;
-}
-
-console.log(bill([10, 15, 30], 0.2));
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);

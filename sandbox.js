49113a77d6b67b8b5d7a01ce02e9e88ae31eebdd
Gowthamap <gowtham.p@vonnue.com>
2025-07-27 16:17:00 +0530

    feat: objects and array of objects in javascript

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,43 @@
 // object literals
 
+const blogs = [
+  {
+    title: "why mac & cheese rules",
+    likes: 30,
+  },
+  { title: "10 things to make with marmite", likes: 50 },
+];
+
+// console.log(blogs);
+
 let user = {
   name: "crystal",
   age: 30,
   email: "crystal@thenetninja.co.uk",
   location: "berlin",
-  blogs: ["why mac & cheese rules", "10 things to make with marmite"],
+  blogs: [
+    {
+      title: "why mac & cheese rules",
+      likes: 30,
+    },
+    { title: "10 things to make with marmite", likes: 50 },
+  ],
+  login: function () {
+    console.log("the user logged in");
+  },
+  logout: function () {
+    console.log("the user logged out");
+  },
+  logBlogs: function () {
+    // // console.log(this.blogs);
+    console.log("this use has written the following blogs:");
+    this.blogs.forEach((blog) => {
+      console.log(blog.title, blog.likes);
+    });
+  },
 };
 
-console.log(user);
-
-console.log(user.name);
-// user.age = 35;
-console.log(user.age);
-
-const key = "location";
-
-console.log(user[key]);
-console.log(user["location"]);
-user["name"] = "chun-li";
-console.log(user["name"]);
-
-console.log(typeof user);
+// user.login();
+// user.logout();
+user.logBlogs();
+console.log(this);

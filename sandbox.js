3b9cee057dbe61511f92ff8ae9fe4b070990d7b5
Gowthamap <gowtham.p@vonnue.com>
2025-08-22 16:12:46 +0530

    feat: document.getElementById(), document.getElementsByClassName() and document.getElementsByTagName()

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,13 @@
-// const para = document.querySelector("div.error");
+// get an element by ID
+// const title = document.getElementById("page-title");
+// console.log(title);
 
-// console.log(para);
+// get element by their class name
+// const errors = document.getElementsByClassName("error");
+// console.log(errors);
+// console.log(errors[0]);
 
-const paras = document.querySelectorAll("p");
-
-const errors = document.querySelectorAll(".error");
-
-console.log(errors);
+// get elements by their tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+console.log(paras[1]);

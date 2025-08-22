730031c9280bbfca77f751123c6d5f913204f7a5
Gowthamap <gowtham.p@vonnue.com>
2025-08-22 15:59:20 +0530

    feat: document.querySelector() and document.querySelectorAll()

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,9 @@
-// primitive values
+// const para = document.querySelector("div.error");
 
-// let scoreOne = 50;
-// let scoreTwo = scoreOne;
+// console.log(para);
 
-// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+const paras = document.querySelectorAll("p");
 
-// scoreOne = 100;
-// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+const errors = document.querySelectorAll(".error");
 
-// reference values
-
-const userOne = { name: "ryu", age: 30 };
-const userTwo = userOne;
-
-console.log(userOne, userTwo);
-
-userOne.name = "chun-li";
-console.log(userOne, userTwo);
+console.log(errors);
